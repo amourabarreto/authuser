b0696109cb01765f5254ec37e07f486e8bbee0c8
amourabarreto <amourabarreto@gmail.com>
2022-04-16 18:29:16 -0300

    DockFile

--- src/main/java/com/ead/authuser/repositories/UserCourseRepository.java
+++ src/main/java/com/ead/authuser/repositories/UserCourseRepository.java
@@ -4,6 +4,7 @@ import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,5 +16,5 @@ public interface UserCourseRepository extends JpaRepository<UserCourseModel, UUI
     boolean existsByUserAndCourseId(UserModel userModel, UUID courseId);
 
     @Query(value = "SELECT userCourseModel from UserCourseModel userCourseModel where userCourseModel.user.userId = :userId")
-    List<UserCourseModel> findAllUserCourseIntoCourse(UUID userId);
+    List<UserCourseModel> findAllUserCourseIntoCourse(@Param("userId") UUID userId);
 }
