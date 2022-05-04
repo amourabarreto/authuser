b735a8364be8a62f97e2844d11e6cbcfd26fb321
Andre Barreto <amourabarreto@gmail.com>
2022-05-04 20:56:39 -0300

    Autenticação JWT Finalizado - USERID

--- src/main/java/com/ead/authuser/controllers/InstructorController.java
+++ src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -1,8 +1,11 @@
 package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.InstructorDto;
+import com.ead.authuser.enums.RoleType;
 import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.RoleModel;
 import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.RoleService;
 import com.ead.authuser.services.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +27,9 @@ public class InstructorController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    RoleService roleService;
+
     @PreAuthorize("hasAnyRole('INSTRUCTOR')")
     @PostMapping("/subscription")
     public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDto instructorDto) {
@@ -32,8 +38,10 @@ public class InstructorController {
         if (userModelOptional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado!");
         } else {
+            RoleModel roleModel= roleService.findByRoleName(RoleType.ROLE_INSTRUCTOR).orElseThrow(() -> new RuntimeException("Error : Role is not found!"));
             var userModel = userModelOptional.get();
             userModel.setUserType(UserType.INSTRUCTOR);
+            userModel.getRoles().add(roleModel);
             userService.updateUser(userModel);
             return ResponseEntity.status(HttpStatus.OK).body(userModel);
         }
