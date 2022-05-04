5b92a3da02683fccca06a8d2250794d078efd2a0
Andre Barreto <amourabarreto@gmail.com>
2022-05-04 10:36:26 -0300

    Autenticação JWT Finalizado

--- src/main/java/com/ead/authuser/configs/security/JwtProvider.java
+++ src/main/java/com/ead/authuser/configs/security/JwtProvider.java
@@ -1,14 +1,14 @@
 package com.ead.authuser.configs.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
-
+@Log4j2
 @Component
 public class JwtProvider {
 
@@ -26,5 +26,27 @@ public class JwtProvider {
                 .setExpiration(new Date( (new Date()).getTime() + jwtExpirationMs ))
                 .signWith(SignatureAlgorithm.HS512,jwtSecret)
                 .compact();
+    }
+
+    public String getUsernameJwt(String token){
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwt(String token){
+       try {
+           Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+           return true;
+       }catch(SignatureException e){
+           log.error("Invalide JWT Signature: {}",e.getMessage());
+       } catch (MalformedJwtException e){
+           log.error("Invalide JWT Token: {}",e.getMessage());
+       }catch(ExpiredJwtException e){
+           log.error("JWT Token Expired: {}",e.getMessage());
+       } catch (UnsupportedJwtException e){
+           log.error("JWT Unsupported: {}",e.getMessage());
+       } catch(IllegalArgumentException e){
+           log.error("JWT claims string is empity: {}",e.getMessage());
+       }
+     return false;
     }
 }
--- src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
+++ src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
@@ -3,19 +3,18 @@ package com.ead.authuser.configs.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
-
-import javax.transaction.Transactional;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -24,23 +23,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String[] AUTH_WHITELIST ={"/auth/**"};
 
     @Autowired
-    private UserDetailServiceImpl userDetailsService;
+    private UserDetailsServiceImpl userDetailsService;
     @Autowired
     private AuthenticationEntryPoint autenticationEntryPoint;
+
+    @Bean
+    protected AuthenticationJwtFilter authenticationJwtFilter(){
+        return new AuthenticationJwtFilter();
+    }
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .httpBasic()
+                .exceptionHandling()
                 .authenticationEntryPoint(autenticationEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
                 .antMatchers(AUTH_WHITELIST).permitAll()
-                .antMatchers(HttpMethod.GET,"/users/**").hasRole("STUDENT")
                 .anyRequest().authenticated()
                 .and()
-                .csrf().disable()
-                .formLogin();
+                .csrf().disable();
+        httpSecurity.addFilterBefore(authenticationJwtFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
